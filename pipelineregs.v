7f2da48432b7ed403a802a0fb9c61d281d2fc13a
RainSure <1114560341@qq.com>
2022-07-29 19:08:17 +0800

    Update pipelineregs.v

--- pipelineregs.v
+++ pipelineregs.v
@@ -1,39 +1,51 @@
 `timescale 1ns / 1ns
 module preg1(
-input clk,
-input en,
-input reset,
-input in,
-output reg out=0
+	input 		clk,
+	input 		en,
+	input 		reset,
+	input 		in,
+	output reg  out = 0
 );
     always @(posedge clk) begin
-	     if(reset) out=0;
-		  else if(en) out=in;
-	 end
+	    if(reset) begin 
+			out = 0;
+		end
+		else if(en) begin 
+			out = in;
+		end
+	end
 endmodule
 
 module preg5(
-input clk,
-input en,
-input reset,
-input [4:0]in,
-output reg [4:0]out=0
+	input 		clk,
+	input 		en,
+	input 		reset,
+	input [4:0] in,
+	output reg [4:0] out = 0
 );
     always @(posedge clk) begin
-	     if(reset) out=0;
-		  else if(en) out=in;
-	 end
+	    if(reset) begin 
+			out = 0;
+		end
+		else if(en) begin 
+			out = in;
+		end
+	end
 endmodule
 
 module preg32(
-input clk,
-input en,
-input reset,
-input [31:0]in,
-output reg [31:0]out=0
+	input 		clk,
+	input 		en,
+	input 		reset,
+	input [31:0] in,
+	output reg [31:0] out = 0
 );
     always @(posedge clk) begin
-	     if(reset) out=0;
-		  else if(en) out=in;
-	 end
+	    if(reset) begin 
+			out = 0;
+		end
+		else if(en) begin 
+			out = in;
+		end
+	end
 endmodule

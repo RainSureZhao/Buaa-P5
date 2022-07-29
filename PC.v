53072a672b7271415904003c3e052278da09646e
RainSure <1114560341@qq.com>
2022-07-29 19:05:28 +0800

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,13 +1,17 @@
 `timescale 1ns / 1ns
 module PC(
-input clk,
-input en,
-input reset,
-input [31:0]next,
-output reg [31:0]IAddr=32'h00003000
+	input 				clk,
+	input 				en,
+	input 				reset,
+	input [31:0]		next,
+	output reg [31:0]	IAddr = 32'h00003000
 );
     always @(posedge clk) begin
-	     if(reset) IAddr<=32'h00003000;
-		  else if(en) IAddr<=next;
-	 end
+	    if(reset) begin
+			IAddr <= 32'h00003000;
+		end
+		else if(en) begin
+			IAddr <= next;
+		end
+	end
 endmodule

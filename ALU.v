a5097b35758917b01e7a75fb195892f0e396dc42
RainSure <1114560341@qq.com>
2022-07-29 17:50:50 +0800

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,32 +1,62 @@
 `timescale 1ns / 1ns
 module ALU(
-input [31:0]op1,
-input [31:0]op2,
-input [3:0]sel,
-output [31:0]result
+	input [31:0]op1,
+	input [31:0]op2,
+	input [3:0]sel,
+	output [31:0]result
 //output zero
 );
     reg [31:0]res;
-	 always @(*) begin
-	     case(sel)
-		      0:res=op1;//buffer
+	 	always @(*) begin
+	     	case(sel)
+		      	0 : begin 
+					res = op1;//buffer
+				end
 				//1:res={31'b0,op1[31]};//<0
-				2:res=op1+op2;
-				3:res=op1-op2;
-				4:res=op1&op2;
-				5:res=op1|op2;
-				6:res=op1^op2;
-				7:res=~(op1|op2);
-				8:res=op2>>(op1[4:0]);
-				9:res=($signed(op2))>>>(op1[4:0]);
-				10:res=op2<<(op1[4:0]);
+				2 : begin 
+					res = op1 + op2;
+				end
+				3 : begin 
+					res = op1 - op2;
+				end
+				4 : begin 
+					res = op1 & op2;
+				end
+				5 : begin 
+					res = op1 | op2;
+				end
+				6 : begin 
+					res = op1 ^ op2;
+				end
+				7 : begin 
+					res = ~ (op1 | op2);
+				end
+				8 : begin 
+					res = op2 >> (op1[4:0]);
+				end
+				9 : begin 
+					res = ($signed(op2)) >>> (op1[4:0]);
+				end
+				10 : begin 
+					res = op2 << (op1[4:0]);
+				end
 				//11:res={31'b0,(op1==op2)};
-				11:res=(op2>>(op1[4:0]))|(op2<<(5'b0-op1[4:0]));
-				12:res={31'b0,($signed(op1))<($signed(op2))};
-				13:res={31'b0,op1<op2};
+				11 : begin 
+					res = (op2 >> (op1[4:0])) | (op2 << (5'b0 - op1[4:0]));
+				end
+				12 : begin 
+					res = {31'b0, ($signed(op1)) < ($signed(op2))};
+				end
+				13 : begin 
+					res = {31'b0, op1 < op2};
+				end
 				//14:res={31'b0,op1!=0&&(~op1[31])};//>0
-				14:res=(op2<<(op1[4:0]))|(op2>>(5'b0-op1[4:0]));
-				//15:res={31'b0,op1==0||op1[31]};//<=0
+				14 : begin 
+					res = (op2 << (op1[4:0])) | (op2 >> (5'b0 - op1[4:0]));
+				end
+				15 : begin 
+					res = {31'b0, op1 == 0 || op1[31]};//<=0
+				end
 		  endcase
 	 end
 	 assign result=res;
